1a192b5f16b3869d5a48c121109ae53756cb863f
Rohithraj16 <168125585+Rohithraj16@users.noreply.github.com>
2025-03-15 02:07:54 +0530

    gemini AI :successfully loading the content the main page

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,14 +1,39 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+	const [input, setInput] = useState("");
+	const [recentPrompt, setRecentprompt] = useState("");
+	const [prevPrompts, setPrevPrompts] = useState([]);
+	const [showResult, setShowResult] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [resultData, setResultData] = useState("");
 	const onSent = async (prompt) => {
-		await run(prompt);
+		setResultData("");
+		setLoading(true);
+		setShowResult(true);
+		setRecentprompt(input);
+		const response = await run(input);
+		setResultData(response);
+		setLoading(false);
+		setInput("");
 	};
-	onSent("what is react js");
-	const contextValue = {};
+
+	const contextValue = {
+		prevPrompts,
+		setPrevPrompts,
+		onSent,
+		setRecentprompt,
+		recentPrompt,
+		showResult,
+		loading,
+		resultData,
+		input,
+		setInput,
+		//newChat
+	};
 	return (
 		<Context.Provider value={contextValue}>{props.children}</Context.Provider>
 	);
